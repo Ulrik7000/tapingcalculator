81364306a4df5f1081dbd0541a64e51c6cf30fa1
Ulrik7000 <82905858+Ulrik7000@users.noreply.github.com>
2021-05-30 16:55:15 -0400

    html

--- appDM.js
+++ appDM.js
@@ -1,48 +1,71 @@
-//jshint esversion:6
+// jshint esversion: 6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const request = require("request");
+const https = require("https");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
- //above is default template//
- app.get("/", function(req, res){
-   res.render("index.ejs");
- });
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/DMsignup.html");
+})
 
- app.get("/calcsheet", function(req, res){
-   res.render("calcsheet.ejs");
- });
+app.post("/", function (req, res) {
 
- app.get("/calcsqft", function(req, res){
-   res.render("calcsqft.ejs");
- });
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
- app.get("/calcrep", function(req, res){
-   res.render("calcrep.ejs");
- });
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "mailchimpurl";
+
+  const options = {
+    method: "POST",
+    auth: "random:mailchimpauth"
+  }
+
+const request =https.request(url, options, function(response) {
+
+  if (response.statusCode === 200) {
+    res.sendFile(__dirname + "/success.html");
+  } else {
+    res.sendFile(__dirname + "/failure.html");
+  }
+
+ response.on("data", function(data){
+  console.log(JSON.parse(data));
+ })
+})
+
+request.write(jsonData);
+request.end();
+
+});
 
 
+app.post("/failure", function(req, res) {
+  res.redirect("/")
+})
 
 
-
-
-
-
-
-
-
-app.listen(4000, function() {
-  console.log("Server started on port 4000");
+app.listen(process.env.PORT || 4000, function() {
+  console.log("Server is running on port 4000");
 });
